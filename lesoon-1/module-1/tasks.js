36a94608b7cb9072817563f4d86757b8ce1020c3
YuliaPine7 <yuliasosnina22@gmail.com>
2023-04-08 12:54:25 +0100

    Tasks 1-3 completed

--- lesoon-1/module-1/tasks.js
+++ lesoon-1/module-1/tasks.js
@@ -1,14 +1,58 @@
 //TODO: 1 ===================================
 // Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
 
+// const a = Number(prompt('Enter your number'))
+// if(a === 10){
+//     alert('Вірно')
+// }
+// else{
+//    alert ('Невірно')
+// }
+// a === 10 ? alert('Вірно') : alert('Невірно');
+
+
 //TODO: 2 ===================================
 // У змінній min лежить число від 0 до 59. Визначте у котру чверть години
 // Попадає це число (у першу, другу, третю або четверту).
+
+// const min = +prompt('Enter your number');
+// console.log(min)
+
+// if(min > 0 && min <= 15){
+//     alert('First quarter')
+// } else if(min > 15 && min <= 30){
+//     alert('Second quarter')
+// } else if(min > 30 && min <= 45){
+//     alert('Third quarter')
+// } else if(min > 45 && min < 60){
+//     alert('Fourth quarter')
+// } else{
+//     alert('Enter a different number')
+// }
 
 //TODO: 3 ===================================
 // Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
 // значення '1', то у змінну result запишемо 'зима', якщо має значення
 // '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+
+// const num = +prompt('Enter your number');
+
+// switch(num){
+//     case 1:
+//         alert('Winter');
+//         break;
+//     case 2:
+//         alert('Spring');
+//         break;
+//     case 3:
+//         alert ('Summer');
+//         break;
+//     case 4 :
+//         alert('Autumn');
+//         break;
+//     default:
+//         alert('Enter a different number');
+// }
 
 //TODO: 4 ===================================
 // скористаємося циклом while та виведіть у консоль числа від 0 до 50
